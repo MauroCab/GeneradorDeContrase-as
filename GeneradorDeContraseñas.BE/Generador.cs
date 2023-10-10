ab3a572ac71843e3b7e4d348990ffa013c91322c
MauroCab <135880588+MauroCab@users.noreply.github.com>
2023-10-10 12:04:02 -0300

    Add files via upload

---
+++ GeneradorDeContraseñas.BE/Generador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorDeContraseñas.BE
+{
+    public class Generador
+    {
+        private const string CarMinusculas = "abcdefghijklmnopqrstuvwxyz";
+        private const string CarMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string CarNumeros = "0123456789";
+        private const string CarSimbolos = "!¡¿@#$%^&*()_-+=<>?";
+
+
+        public string GenerarContraseña(int longitud,
+                                        bool incluirMayusculas,
+                                        bool incluirNumeros,
+                                        bool incluirSimbolos)
+        {
+            StringBuilder contrasena = new StringBuilder();
+            string caracteres = CarMinusculas;
+
+            if (incluirMayusculas == true)
+            {
+                caracteres += CarMayusculas;
+            }
+
+            if (incluirNumeros == true)
+            {
+                caracteres += CarNumeros;
+            }
+
+            if (incluirSimbolos == true)
+            {
+                caracteres += CarSimbolos;
+            }
+
+            Random random = new Random();
+
+            for (int i = 0; i == longitud; i++)
+            {
+                int indice = random.Next(caracteres.Length);
+                contrasena.Append(caracteres[indice]);
+            }
+
+            return contrasena.ToString();
+
+        }
+    }
+}
---
+++ GeneradorDeContraseñas.FE/GeneradorDeContraseñaFrm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GeneradorDeContraseñas.BE;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneradorDeContraseñas.FE
+{
+    public partial class Formulario1 : Form
+    {
+        public Formulario1()
+        {
+            InitializeComponent();
+        }
+
+        public void btGenerar_Click(object sender, EventArgs e)
+        {
+            Generador generador = new Generador();
+
+            int longitud = Convert.ToInt32(txtbxLongitud.Text);
+
+            bool incluirMayusculas = chkMayusculas.Checked;
+            bool incluirNumeros = chkNumeros.Checked;
+            bool incluirSimbolos = chkSimbolos.Checked;
+
+            string contraseña = generador.GenerarContraseña(longitud, incluirMayusculas, incluirNumeros, incluirSimbolos);
+
+            lblResultado.Text = "La contraseña generada es" + contraseña;
+            
+
+        }
+
+        
+    }
+}
